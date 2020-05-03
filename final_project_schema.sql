0ac576c6e5e2106a507346671039a6f73720523b
Avahayako <avahayako>
2020-05-03 00:03:08 -0700

    final edit hopefully >~<

--- final_project_schema.sql
+++ final_project_schema.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE if not exists FINAL_PROJECT;
 USE FINAL_PROJECT;
 
-
-
 create table if not exists Subscriber (
 	subID int primary key,
 	subName varchar(50)
@@ -10,11 +8,12 @@ create table if not exists Subscriber (
 
 create table if not exists Artist (
 	aID int primary key,
-	aName varchar(50)
+	aName varchar(50),
+    follower_count int DEFAULT 0
 );
 
 create table if not exists Playlist (
-	pID int primary key,
+	pID varchar(50) primary key,
     subID int,
 	pName varchar(50),
     privacy boolean,
@@ -22,10 +21,11 @@ create table if not exists Playlist (
 );
 
 create table if not exists Song (
-	sID int primary key,
-    aid int,
+	sID varchar(50) primary key,
+    aID int,
     sName varchar(50),
     publish_date date,
+    like_count int DEFAULT 0,
     FOREIGN KEY (aID) REFERENCES Artist(aID)
     );
 
@@ -38,8 +38,8 @@ create table if not exists Song (
 );
 
 create table if not exists Is_On (
-	sid int,
-    pid int,
+	sid varchar(50),
+    pid varchar(50),
 	PRIMARY KEY (sID, pID),
 	FOREIGN KEY (sID) REFERENCES Song(sID),
     FOREIGN KEY (pID) REFERENCES Playlist(pID)
@@ -51,26 +51,12 @@ insert into Subscriber values (2, 'AvaHayako');
 insert into Subscriber values (3, 'MaryMoki');
 insert into Subscriber values (4, 'Johnny AppleSeed');
 
-insert into Artist values (101, 'San Holo');
-insert into Artist values (102, 'Raveena Aurora');
+insert into Artist values (101, 'San Holo',0);
+insert into Artist values (102, 'Raveena Aurora',0);
 
-insert into Playlist values(110, 1, 'Liked', False);
-insert into Playlist values(120, 2, 'Happy Songs', False);
-insert into Playlist values(130, 3, 'Liked', True);
-insert into Playlist values(140, 4, 'Upbeat', False);
+insert into Song values("102SweetTime", 102, 'Sweet Time', 06/10/2018, 0);
+insert into Song values("102Stronger", 102, 'Stronger', 07/15/2019, 0);
+insert into Song values("102Petal", 102, 'Petal', 07/15/2019, 0);
+insert into Song values("101WeRise", 101, 'We Rise', 06/15/2015, 0);
+insert into Song values("101liftmefromtheground", 101, 'lift me from the ground', 03/11/2018, 0);
 
-insert into Song values(111, 102, 'Sweet Time', 06/10/2018);
-insert into Song values(112, 102, 'Stronger', 07/15/2019);
-insert into Song values(113, 102, 'Petal', 07/15/2019);
-insert into Song values(114, 101, 'We Rise', 06/15/2015);
-insert into Song values(115, 101, 'lift me from the ground', 03/11/2018);
-insert into Song values(116, 101, 'I Still See Your Face', 04/15/2017);
-
-
-
-insert into Follow values(1, 101);
-insert into Follow values(2,102);
-
-insert into Is_on values (111,120);
-insert into Is_on values (112,120);
-insert into Is_on values (113,120);
